d803640bff973764c2c8fdb93e1f2bab1bd9a51f
pyaephyo <soemyatmyat@openmailbox.org>
2016-01-11 00:16:44 +0800

    production env

--- config/env/development.js
+++ config/env/development.js
@@ -1,9 +1,9 @@
-var port = process.env.PORT;
+var port = 8000;
 
 module.exports = {
    port: port,
-   db_host: "us-cdbr-iron-east-03.cleardb.net",
-   db_user: "b64b4d62e37e3a",
-   db_password: "2378ace4",
-   db_database: "heroku_4e3503cbba1b2e6"
+   db_host: "localhost",
+   db_user: "root",
+   db_password: "password",
+   db_database: "SMUX"
 };
