8b5a913f04976f3b7407f5abea946c5786929340
pyaephyo <soemyatmyat@openmailbox.org>
2016-02-09 21:11:02 +0800

    sql pool

--- config/env/development.js
+++ config/env/development.js
@@ -1,18 +1,9 @@
 var port = 8000;
 
-/*
 module.exports = {
    port: port,
    db_host: "localhost",
    db_user: "root",
    db_password: "password",
    db_database: "SMUX"
-};*/
-
-module.exports = {
-   port: port,
-   db_host: "us-cdbr-iron-east-03.cleardb.net",
-   db_user: "b64b4d62e37e3a",
-   db_password: "2378ace4",
-   db_database: "heroku_4e3503cbba1b2e6"
 };
